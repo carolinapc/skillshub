9ba3fc0eb0a67fa8fed7cb39264e1e95bd9131a9
Carolina Cavalcanti <carolinapc@gmail.com>
2019-11-12 15:06:38 -0500

    added the modal before start contact

--- client/src/pages/Skill/index.jsx
+++ client/src/pages/Skill/index.jsx
@@ -5,13 +5,15 @@ import PageContainer from '../../components/PageContainer';
 import { Row, Col, Button } from 'react-bootstrap';
 import Reviews from './Reviews';
 import ReviewForm from './ReviewForm';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import ContactModal from './ContactModal';
 
 class Skill extends React.Component {
   mounted = false;
 
   state = {
     skill: {
+      id: "",
       name: "",
       price: "",
       priceType: "",
@@ -24,7 +26,9 @@ class Skill extends React.Component {
       Reviews: []
     },
     found: false,
-    loggedin: false
+    loggedin: false,
+    contactText: "",
+    showContactModal: false
   };
   
   componentDidMount = () => {
@@ -58,17 +62,21 @@ class Skill extends React.Component {
 
   contactHandle = () => {
 
+    //checks if the contact is already opened (or still opened)
     API.getSkillContact(this.state.skill.id).then(res => {
+      //if it was found
       if (res.data) {
+        //redirect to the requests page with the opened chat
         this.props.history.push(`/contact/request/${res.data.id}`);
       }
       else {
-        API.createSkillContact({ SkillId: this.state.skill.id, text: "first contact" }).then(res => {
-          this.props.history.push(`/contact/request/${res.data.id}`);
-        }).catch(err => console.log(err.response));
+        //opens a modal to create a new contact
+        this.setState({ showContactModal: true });
       }
     }).catch(err => console.log(err));
   }
+
+  closeContactModal = () => this.setState({ showContactModal: false });
 
   render() { 
     const { skill, found } = this.state;
@@ -76,58 +84,67 @@ class Skill extends React.Component {
     return (
       <>
       {found?
-      <PageContainer title="">
-        <Row className="mt-5">
-          <Col md="4">
-            <img src={skill.User.image? `/${skill.User.image}` : "/profile.jpg"} alt="Profile" className="profile-img shadow-lg mb-4" />
-            <h3 className="card-subtitle mb-2 text-muted">{skill.User.firstName + " " + skill.User.lastName}</h3>
-            <NavLink
-              exact
-              to={"/profile/"+skill.UserId}
-              activeClassName="active"
-              className="btn btn-primary mr-3"
-              >
-              <i className="far fa-user-circle"></i> View Profile
-            </NavLink>
-            {(this.state.loggedin || this.props.userData.loggedin) ?
-              <Button
-                className="btn btn-primary"
-                onClick={this.contactHandle}
-              >
-                <i className="far fa-comments"></i> Contact
-              </Button>
-            :
-              <Button className="btn-secondary mr-3" onClick={() => this.props.toggleAuthModalShow("signin")}>Sign-In to Contact</Button>
-            }
-          </Col>
-          <Col md="8" className="pt-3">
-            <div className="skill-header">
-              <h4>{skill.name}</h4>
-              <div>
-                <h4>Price: ${skill.price + " per " + Utils.getPriceTypeName(skill.priceType)}</h4>
-                <h5>{skill.score}.0 {Utils.getStars(skill.score)} ({skill.Reviews.length})</h5>  
-              </div>
-            </div>    
+        <PageContainer title="">
+          <Row className="mt-5">
+            <Col md="4">
+              <img src={skill.User.image? `/${skill.User.image}` : "/profile.jpg"} alt="Profile" className="profile-img shadow-lg mb-4" />
+              <h3 className="card-subtitle mb-2 text-muted">{skill.User.firstName + " " + skill.User.lastName}</h3>
+              <NavLink
+                exact
+                to={"/profile/"+skill.UserId}
+                activeClassName="active"
+                className="btn btn-primary mr-3"
+                >
+                <i className="far fa-user-circle"></i> View Profile
+              </NavLink>
+              {(this.state.loggedin || this.props.userData.loggedin) ?
+                <Button
+                  className="btn btn-primary"
+                  onClick={this.contactHandle}
+                >
+                  <i className="far fa-comments"></i> Contact
+                </Button>
+              :
+                <Button className="btn-secondary mr-3" onClick={() => this.props.toggleAuthModalShow("signin")}>Sign-In to Contact</Button>
+              }
+            </Col>
+            <Col md="8" className="pt-3">
+              <div className="skill-header">
+                <h4>{skill.name}</h4>
+                <div>
+                  <h4>Price: ${skill.price + " per " + Utils.getPriceTypeName(skill.priceType)}</h4>
+                  <h5>{skill.score}.0 {Utils.getStars(skill.score)} ({skill.Reviews.length})</h5>  
+                </div>
+              </div>    
 
-            <div className="skill-description">{Utils.replaceNewLines(skill.description)}</div>
-            
-          </Col>
-        </Row>
-        <Row className="border-top mt-4 p-2">
-          <Col md="4">
-            {(this.state.loggedin || this.props.userData.loggedin )?
-            <ReviewForm skillId={skill.id} getSkillFromDb={this.getSkillFromDb} />
-            : "Sign in to add a review"
-            }
-          </Col>            
-          <Col md="8">
-            <h3>Reviews</h3>
-            
-            <Reviews reviews={skill.Reviews}/>
-          </Col>
-        </Row>  
-      </PageContainer>
-      : <PageContainer title="Skill was not found!" />}
+              <div className="skill-description">{Utils.replaceNewLines(skill.description)}</div>
+              
+            </Col>
+          </Row>
+          <Row className="border-top mt-4 p-2">
+            <Col md="4">
+              {(this.state.loggedin || this.props.userData.loggedin )?
+              <ReviewForm skillId={skill.id} getSkillFromDb={this.getSkillFromDb} />
+              : "Sign in to add a review"
+              }
+            </Col>            
+            <Col md="8">
+              <h3>Reviews</h3>
+              
+              <Reviews reviews={skill.Reviews}/>
+            </Col>
+          </Row>  
+        </PageContainer>
+      :
+        <PageContainer title="Skill was not found!" />
+      }
+        
+      <ContactModal
+        handleCloseModal={this.closeContactModal}
+        show={this.state.showContactModal}
+        skillId={skill.id}
+        {...this.props}
+      />
     </>
     );
   }
